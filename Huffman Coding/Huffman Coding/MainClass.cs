90a00300d8afe7d2f996d3875f175d4f3a6079cb
Muhammad Saqlain Tahir <saqlain.tahir56@gmail.com>
2019-11-30 22:42:07 +0500

    Implementation

---
+++ Huffman Coding/Huffman Coding/MainClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman_Coding
+{
+    class MainClass
+    {
+        public static void setColor()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+
+        public static void setColorDefault()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void Main(string[] args)
+        {
+            List<ImpNode> node;
+            Tree ptree = new Tree();
+            while(true)
+            {
+                Console.Clear();
+                node = ptree.getlistfromfile();
+                Console.Clear();
+                if(node==null)
+                {
+                    Console.WriteLine("File canoot be read! ");
+                    Console.WriteLine("Press the any key to continue or Enter \"E\" to exit the program");
+                    string c = Console.ReadLine();
+                    if (c.ToLower() == "e")
+                    {
+                        break;
+                    }
+                    else
+                        continue;
+
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Symbols  .... Frequency");
+                    ptree.info(node);
+                    ptree.Treelist(node);
+                    ptree.Code("", node[0]);
+                    Console.WriteLine(" Huffman Code Created Tree \n");
+                    ptree.printtree(0, node[0]);
+                    Console.WriteLine("Symbols-------Code");
+                    ptree.codesinleaf(node[0]);
+                    Console.WriteLine("/n");
+                    Console.WriteLine("Press the any key to continue or Enter \"E\" to exit the program");
+                    string name = Console.ReadLine();
+                    if (name.ToLower()=="e")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                }
+            }
+        }
+
+    }
+}
---
+++ Huffman Coding/Huffman Coding/Tree.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace Huffman_Coding
+{
+    class Tree
+    {
+        public List<ImpNode> getlistfromfile()        
+        {
+            List<ImpNode> node = new List<ImpNode>();
+             
+            string name = @"C:\Users\I'm Libra\source\repos\Huffman Coding\tree.txt";
+
+
+
+            try
+            {
+                FileStream file = new FileStream(name, FileMode.Open, FileAccess.Read);
+                for(int j=0;j<file.Length;j++)
+                {
+                    string onlyread = Convert.ToChar(file.ReadByte()).ToString();
+                    if (node.Exists(x => x.symbol == onlyread))
+                        node[node.FindIndex(y => y.symbol == onlyread)].increaseinFrequency();
+                    else
+                        node.Add(new ImpNode(onlyread));
+                }
+                node.Sort();
+                return node;
+            }
+            catch(Exception)
+            {
+                return null;
+            }
+        }
+        public void Treelist(List<ImpNode> nodes)
+        {
+            while(nodes.Count>1)
+            {
+                ImpNode node = nodes[0];
+                nodes.RemoveAt(0);
+                ImpNode n = nodes[0];
+                nodes.RemoveAt(0);
+                nodes.Add(new ImpNode(node, n));
+                nodes.Sort();
+            }
+        }
+        public void Code(string code, ImpNode node)
+        {
+            if (node==null)
+            {
+                return;
+            }
+            if (node.left == null && node.right== null)
+            {
+                node.code = code;
+                return;
+            }
+            Code(code + "0", node.left);
+            Code(code + "1", node.right);
+
+
+
+        }
+        public void printtree(int level, ImpNode node)
+        {
+            if (node== null)
+            {
+                return;
+            }
+            for(int k=0;k<level;k++)
+            {
+                Console.Write("\t");
+            }
+            Console.Write("[" + node.symbol + "]");
+            Console.WriteLine("(" + node.code + ")");
+            printtree(level + 1, node.right);
+            printtree(level + 1, node.left);
+        }
+        public void info(List<ImpNode> node)
+        {
+            foreach(var x in node)
+            {
+                Console.WriteLine("Symbol: {0} - Frequency : {1}", x.symbol, x.frequency);
+            }
+        }
+        public void codesinleaf(ImpNode node)
+        {
+            if (node==null)
+            {
+                return;
+            }
+            if (node.left == null && node.right == null)
+            {
+                Console.WriteLine("Symbol : {0} -  Code : {1}", node.symbol, node.code);
+                return;
+            }
+            codesinleaf(node.left);
+            codesinleaf(node.right);
+        }
+    }
+}
