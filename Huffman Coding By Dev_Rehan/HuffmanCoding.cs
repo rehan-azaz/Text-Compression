54abb8310beef7fafc550c363be3dc9cfc7db44d
Muhammad Saqlain Tahir <saqlain.tahir56@gmail.com>
2019-12-09 11:09:29 +0500

    Code

---
+++ Huffman Coding By Dev_Rehan/HuffmanCoding.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuffmanCoding
+{
+    public class HuffmanCoding
+    {
+        PriorityQueue<string> Q = new PriorityQueue<string>();
+       List<HuffmanNode> nList = new List<HuffmanNode>();
+
+        public void Compress(string text)
+        {
+            HuffmanNode node = new HuffmanNode();
+            List<HuffmanNode> nodeList = node.GetList(text);
+            PriorityQueue<string> PQ = new PriorityQueue<string>();
+            foreach (HuffmanNode n in nodeList)
+            {
+                PQ.Enqueue(n);
+            }
+            int size = PQ.Size;
+            while (size >= 1)
+            {
+                HuffmanNode hNode = new HuffmanNode();
+                HuffmanNode a = new HuffmanNode();
+                HuffmanNode b = new HuffmanNode();
+                if (!PQ.isEmpty())
+                    a = PQ.Dequeue();
+                if (!PQ.isEmpty())
+                   b = PQ.Dequeue();
+                bool isA = true;
+                bool isB = true;
+                foreach (HuffmanNode n in nList)
+                {
+                    if(n == a)
+                    {
+                        isA = false;
+                    }
+                    if (n == b)
+                    {
+                        isB = false;
+                    }
+                }
+
+                if (isA && a.Symbol != null)
+                    nList.Add(a);
+                if(isB && b.Symbol != null)
+                    nList.Add(b);
+
+                hNode.Left = a;
+                hNode.Right = b;
+
+                hNode.Frequency = a.Frequency + b.Frequency;
+                hNode.Symbol = a.Symbol + b.Symbol;
+                hNode.Left.Parent = hNode.Left.Parent = hNode;
+                PQ.Enqueue(hNode);
+                size--;
+            }
+            nList.Reverse();
+            foreach (HuffmanNode n in nList)
+            {
+                AssignCode();
+            }
+
+        }
+
+        void AssignCode()
+        {
+            for (int i = 0; i < nList.Count && nList[i].Left != null && nList[i].Right != null; i++) 
+            {
+                nList[i].Left.Code += "0";
+                nList[i].Right.Code += "1";
+            }
+        }
+        public void Display()
+        {
+            Console.WriteLine("Symbols,\tFrequency");
+            foreach (HuffmanNode n in nList)
+            {
+                Console.WriteLine(n.Symbol +" , "+n.Frequency+"\n");
+            }
+        }
+        public void DisplayCodes()
+        {
+            Console.WriteLine("Symbols,\tFrequency,\tCode");
+            foreach (HuffmanNode n in nList)
+            {
+                Console.WriteLine(n.Symbol + ",\t\t" + n.Frequency+",\t\t"+n.Code + "\n");
+            }
+        }
+    }
+}
---
+++ Huffman Coding By Dev_Rehan/PriorityQueue.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace HuffmanCoding
+{
+    
+    public class PriorityQueue<T> where T : IComparable
+    {
+        
+        int heapSize = -1;
+        List<HuffmanNode> nodes = new List<HuffmanNode>();
+        public int Size
+        {
+            get
+            {
+                return nodes.Count;
+            }
+        }
+        private void Swap(int i, int j)
+        {
+            var temp = nodes[i];
+            nodes[i] = nodes[j];
+            nodes[j] = temp;
+        }
+        private int LeftChild(int i)
+        {
+            return i * 2 + 1;
+        }
+        private int RightChild(int i)
+        {
+            return i * 2 + 2;
+        }
+
+        private void MinHeapify(int i)
+        {
+            int left = LeftChild(i);
+            int right =RightChild(i);
+
+            int smallest = i;
+
+            if (left <= heapSize && nodes[smallest].Frequency > nodes[left].Frequency)
+                smallest = left;
+            if (right <= heapSize && nodes[smallest].Frequency > nodes[right].Frequency)
+                smallest = right;
+
+            if (smallest != i)
+            {
+                Swap(smallest, i);
+                MinHeapify(smallest);
+            }
+        }
+
+        private void BuildHeap(int i)
+        {
+            while (i >= 0 && nodes[(i - 1) / 2].Frequency > nodes[i].Frequency)
+            {
+                Swap(i, (i - 1) / 2);
+                i = (i - 1) / 2;
+            }
+        }
+        public void Enqueue(HuffmanNode n)
+        {
+            nodes.Add(n);
+            heapSize++;
+            BuildHeap(heapSize);
+        }
+        public HuffmanNode Dequeue()
+        {
+            if (heapSize > -1)
+            {
+                var s = nodes[0];
+                nodes[0] = nodes[heapSize];
+                nodes.RemoveAt(heapSize);
+                heapSize--;
+                MinHeapify(0);
+                return s;
+            }
+            else
+                throw new Exception("Queue is empty");
+        }
+        
+        public void Display()
+        {
+            Console.WriteLine("Symbols , Frequency");
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                Console.WriteLine(nodes[i].Symbol+" , "+nodes[i].Frequency);
+            }
+
+        }
+        public bool isEmpty()
+        {
+            return (Size == 0) ? true:false;
+        }
+    }
+}
